95bfb1b9e6434de535a78700912e0bb40178055a
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-09-29 19:38:33 +0300

    test objects

--- NetworkUtility/NetworkUtility/NetworkService.cs
+++ NetworkUtility/NetworkUtility/NetworkService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,6 +20,14 @@ namespace SW.Payroll.NetworkUtility
         public DateTime LastPingDate()
         {
             return DateTime.Now;
+        }
+        public PingOptions GetPingOptions()
+        {
+            return new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
         }
     }
 }
